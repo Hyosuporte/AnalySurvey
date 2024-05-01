cbacdb62d8fecef97c61515c9d4bb1f8590773dc
Hyosuporte <cacuervo120@gmail.com>
2024-05-01 17:26:03 -0500

    test:  prueba de deploy en render #14 back

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,5 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+python manage.py createsuperuser --username admin --email admin@example.com --noinput
