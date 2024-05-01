8d1eedb06a5b8beb311489257be2e1eed8f24eab
Hyosuporte <cacuervo120@gmail.com>
2024-05-01 16:59:01 -0500

    test:  prueba de deploy en render #12 back

--- backend/surveys/apps.py
+++ backend/surveys/apps.py
@@ -1,6 +1,21 @@
+# apps.py
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from .models import TipoPregunta
 
 
-class SurveysConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'surveys'
+
+    def ready(self):
+        @receiver(post_migrate, sender=self)
+        def create_default_tipo_preguntas(sender, **kwargs):
+            if not TipoPregunta.objects.exists():
+                TipoPregunta.objects.bulk_create([
+                    TipoPregunta(tipo='mult'),
+                    TipoPregunta(tipo='text'),
+                    TipoPregunta(tipo='check'),
+                    TipoPregunta(tipo='rating'),
+                ])
