d2482c7f2fe07b19bb36f1cecb09d046f1e3d345
Hyosuporte <cacuervo120@gmail.com>
2024-05-01 02:38:58 -0500

    test:  prueba de deploy en render #10 back

--- backend/surveys/signals.py
+++ backend/surveys/signals.py
@@ -5,7 +5,7 @@ from .models import TipoPregunta
 
 @receiver(post_migrate)
 def create_default_tipo_preguntas(sender, **kwargs):
-    if sender.name == 'your_app_name':  # Reemplaza 'your_app_name' con el nombre de tu aplicación
+    if sender.name == 'analySurveyAPI':  # Reemplaza 'your_app_name' con el nombre de tu aplicación
         if not TipoPregunta.objects.exists():
             TipoPregunta.objects.bulk_create([
                 TipoPregunta(tipo='mult'),
