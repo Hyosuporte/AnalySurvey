0c8b9942bd0ea017dcf814f1411ceb4739b6cf36
hyosuporte <cacuervo120@gmail.com>
2025-05-13 13:11:49 -0500

    feature cracion de root user automaticamente

--- backend/build.sh
+++ backend/build.sh
@@ -8,5 +8,17 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
+python manage.py makemigrations
 python manage.py migrate
 
+# Create root user
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpassword")
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+EOF
